f3a4cfe2a4292f71b84ae111b18a2ac04f46c6ee
Muhammet Harun Demir <muhammetharundemir@gmail.com>
2023-04-21 08:13:10 +0300

    Minor changes.

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
-Spotify Playlist Updater
+# Spotify Playlist Updater
+This software allows you to update your Spotify playlist with the top 40 songs from Radyo ODTÜ.
 
-This software will update your spotify list according to top 40 songs list of Radyo ODTÜ.
-Radyo ODTÜ website: https://www.radyoodtu.com.tr/
-(If you are Radyo ODTÜ and you think I have violated your rights, please contact me: muhammetharundemir@gmail.com)
+## Installation
+Clone the repository:
 
-Using:
-- Requests: https://requests.readthedocs.io/en/latest/
-- Beautiful Soup: https://www.crummy.com/software/BeautifulSoup/
+```bash
+git clone https://github.com/muhammetharundemir/spotify_playlist_update.git
+```
+
+Navigate to the project directory:
+
+```bash
+cd spotify_playlist_update
+```
+
+Install the required packages:
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+Modify the spotify_playlist_update.py file with your own Spotify client ID and client secret:
+
+```python
+spotify_client_id = "your_spotify_client_id"
+spotify_client_secret = "your_spotify_client_secret"
+```
+
+Run the spotify_playlist_update.py file:
+
+```bash
+python spotify_playlist_update.py
+```
+
+## Contributing
+We welcome contributions! If you would like to contribute to this project, please fork the repository and submit a pull request. If you encounter any issues or have suggestions for improvements, please open an issue on the GitHub repository.
+
+If you are Radyo ODTÜ and believe that this software violates your rights, please contact the project owner at muhammetharundemir@gmail.com.
--- spotify_playlist_update.py
+++ spotify_playlist_update.py
@@ -1,38 +1,53 @@
+# Edited by ChatGPT
 import requests
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-spotify_client_id="your_spotify_client_id"
-spotify_client_secret="your_spotify_client_id"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id, client_secret=spotify_client_secret, redirect_uri="http://localhost:8888/callback"))
+# Set up Spotify client information and authentication
+spotify_client_id = "your_spotify_client_id"
+spotify_client_secret = "your_spotify_client_secret"
+redirect_uri = "http://localhost:8888/callback"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=spotify_client_id,
+    client_secret=spotify_client_secret,
+    redirect_uri=redirect_uri
+))
+
+# Get the current user's Spotify username
 sp_user = sp.current_user()
 user_name = sp_user['uri'].split(":")[2]
+
+# Set up headers and URL for web scraping
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-URL = "https://www.radyoodtu.com.tr/40haramiler"
-page = requests.get(url=URL, headers=headers)
+url = "https://www.radyoodtu.com.tr/40haramiler"
+page = requests.get(url=url, headers=headers)
 soup = BeautifulSoup(page.content, "html.parser")
-divArtist = soup.find_all("div", {"class": "artist"})
-divSong = soup.find_all("div", {"class": "song"})
-set_array = {'temporary'}
+
+# Find artist and song information from the website
+div_artist = soup.find_all("div", {"class": "artist"})
+div_song = soup.find_all("div", {"class": "song"})
+
+# Remove duplicate songs from the playlist
 spotify_playlist_id = "your_playlist_id"
 all_songs = sp.playlist_items(playlist_id=spotify_playlist_id)
-
 try:
-    for i in range(0,40):
+    set_array = set()
+    for i in range(0, 40):
         all_songs_link = all_songs['items'][i]['track']['external_urls']['spotify']
         all_songs_id = str(all_songs_link).split("/")[4]
         set_array.add(str(all_songs_id))
-
-    set_array.discard('temporary')
+    set_array.discard('')
     sp.playlist_remove_all_occurrences_of_items(playlist_id=spotify_playlist_id, items=set_array)
-except:
+except KeyError:
+    # Handle KeyError exception if playlist has less than 40 songs
     pass
 
-for i in range(0,40):
-    artist = divArtist[i+5].text.strip()
-    song = divSong[i].text.strip()
-    song = sp.search(q=artist + " " + song, type="track")
+# Add new songs to the playlist
+for i in range(0, 40):
+    artist_name = div_artist[i+5].text.strip()
+    song_name = div_song[i].text.strip()
+    song = sp.search(q=artist_name + " " + song_name, type="track")
     song_link = song['tracks']['items'][0]['external_urls']['spotify']
     song_id = str(song_link).split("/")[4]
     sp.playlist_add_items(playlist_id=spotify_playlist_id, items={song_id})
